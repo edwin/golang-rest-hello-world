2a086430190ae22d6a99134d7ad354bb30e83533
Edwin <edwinkun@gmail.com>
2020-05-23 11:29:39 +0700

    initial commit

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, "{\"message\":\"Hello world!\"}")
+}
